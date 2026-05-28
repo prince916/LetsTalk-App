5fd689efe7406c9d1f9909805c8ed65e5978eaa0
Your Name <kumarprince08081999@gmail.com>
2026-05-28 11:59:57 +0530

    new changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import Signup from "./components/Signup.jsx";
 import Left from "./home/left/Left";
 import Logout from "./home/left1/Logout";
 import Right from "./home/right/Right";
@@ -5,11 +6,12 @@ import Right from "./home/right/Right";
 export default function () {
   return (
     <>
-      <div className="flex h-screen overflow-auto">
+      {/* <div className="flex h-screen overflow-auto">
         <Logout></Logout>
         <Left></Left>
         <Right></Right>
-      </div>
+      </div> */}
+      <Signup ></Signup>
     </>
   );
 }
